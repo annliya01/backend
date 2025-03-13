fcfd15a08820fa52295ab666ee2972942b91f011
annliya01 <anliya2425@gmail.com>
2025-03-13 17:38:17 +0530

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*","djangobackend-jbli.onrender.com"]
+ALLOWED_HOSTS = ["*","http://localhost:3000","djangobackend-jbli.onrender.com"]
 
 
 # Application definition
